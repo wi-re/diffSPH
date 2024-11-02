77e5fe408beae5bdb11160eb6f96d9d4f7fdafeb
Rene <rene.winchenbach@gmail.com>
2024-11-03 00:49:14 +0100

    bscript

--- partiBench/generator/batch.sh
+++ partiBench/generator/batch.sh
@@ -2,4 +2,99 @@ python obstacle_free_slip.py --boundary 'none' --TGV_override TRUE --k 2
 python obstacle_free_slip.py --boundary 'none' --TGV_override TRUE --k 4
 python obstacle_free_slip.py --boundary 'none' --TGV_override TRUE --k 8
 python obstacle_free_slip.py --boundary 'none' --TGV_override TRUE --k 16
-python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --k 32
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 1 --octaves 1
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 1 --octaves 2
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 1 --octaves 3
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 1 --octaves 4
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 2 --octaves 1
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 2 --octaves 2
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 2 --octaves 3
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 2 --octaves 4
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 4 --octaves 1
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 4 --octaves 2
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 4 --octaves 3
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 4 --octaves 4
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 8 --octaves 1
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 8 --octaves 2
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 8 --octaves 3
+python obstacle_free_slip.py --boundary 'none' --TGV_override FALSE --baseFrequency 8 --octaves 4
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override TRUE --k 2
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override TRUE --k 4
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override TRUE --k 8
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override TRUE --k 16
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 1 --octaves 1
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 1 --octaves 2
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 1 --octaves 3
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 1 --octaves 4
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 2 --octaves 1
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 2 --octaves 2
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 2 --octaves 3
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 2 --octaves 4
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 4 --octaves 1
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 4 --octaves 2
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 4 --octaves 3
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 4 --octaves 4
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 8 --octaves 1
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 8 --octaves 2
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 8 --octaves 3
+python obstacle_free_slip.py --boundary 'free-slip' --TGV_override FALSE --baseFrequency 8 --octaves 4
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override TRUE --k 2
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override TRUE --k 4
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override TRUE --k 8
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override TRUE --k 16
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 1 --octaves 1
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 1 --octaves 2
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 1 --octaves 3
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 1 --octaves 4
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 2 --octaves 1
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 2 --octaves 2
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 2 --octaves 3
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 2 --octaves 4
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 4 --octaves 1
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 4 --octaves 2
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 4 --octaves 3
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 4 --octaves 4
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 8 --octaves 1
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 8 --octaves 2
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 8 --octaves 3
+python obstacle_free_slip.py --boundary 'no-slip' --TGV_override FALSE --baseFrequency 8 --octaves 4
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override TRUE --k 2
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override TRUE --k 4
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override TRUE --k 8
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override TRUE --k 16
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 1 --octaves 1
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 1 --octaves 2
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 1 --octaves 3
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 1 --octaves 4
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 2 --octaves 1
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 2 --octaves 2
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 2 --octaves 3
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 2 --octaves 4
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 4 --octaves 1
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 4 --octaves 2
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 4 --octaves 3
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 4 --octaves 4
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 8 --octaves 1
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 8 --octaves 2
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 8 --octaves 3
+python obstacle_free_slip.py --centerObstacle --boundary 'free-slip' --TGV_override FALSE --baseFrequency 8 --octaves 4
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override TRUE --k 2
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override TRUE --k 4
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override TRUE --k 8
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override TRUE --k 16
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 1 --octaves 1
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 1 --octaves 2
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 1 --octaves 3
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 1 --octaves 4
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 2 --octaves 1
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 2 --octaves 2
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 2 --octaves 3
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 2 --octaves 4
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 4 --octaves 1
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 4 --octaves 2
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 4 --octaves 3
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 4 --octaves 4
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 8 --octaves 1
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 8 --octaves 2
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 8 --octaves 3
+python obstacle_free_slip.py --centerObstacle --boundary 'no-slip' --TGV_override FALSE --baseFrequency 8 --octaves 4
